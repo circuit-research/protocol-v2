1c70c4784427aa838070e15f2af5c9ce3e1dd0b4
holygits <beauchjord@gmail.com>
2023-12-22 14:30:41 +0800

    load markets on start up

--- sdk-rs/src/types.rs
+++ sdk-rs/src/types.rs
@@ -45,28 +45,25 @@ impl MarketId {
     ///
     /// Returns an error if symbol and context do not map to a known market
     pub fn lookup(context: Context, symbol: &str) -> Result<Self, ()> {
-        let mut parts = symbol.split('-');
-        match (parts.next(), parts.next()) {
-            (Some(base), None) => {
-                let markets = spot_market_configs(context);
-                if let Some(market) = markets.iter().find(|m| m.symbol.eq_ignore_ascii_case(base)) {
-                    return Ok(MarketId::spot(market.market_index));
-                }
+        if symbol.contains('-') {
+            let markets = perp_market_configs(context);
+            if let Some(market) = markets.iter().find(|m| {
+                unsafe { core::str::from_utf8_unchecked(&m.name) }
+                    .trim_end()
+                    .eq_ignore_ascii_case(symbol)
+            }) {
+                return Ok(MarketId::perp(market.market_index));
             }
-            (Some(base), Some(perp)) => {
-                if perp.eq_ignore_ascii_case("perp") {
-                    let markets = perp_market_configs(context);
-                    if let Some(market) = markets
-                        .iter()
-                        .find(|m| m.base_asset_symbol.eq_ignore_ascii_case(base))
-                    {
-                        return Ok(MarketId::perp(market.market_index));
-                    }
-                }
+        } else {
+            let markets = spot_market_configs(context);
+            if let Some(market) = markets.iter().find(|m| {
+                unsafe { core::str::from_utf8_unchecked(&m.name) }
+                    .trim_end()
+                    .eq_ignore_ascii_case(symbol)
+            }) {
+                return Ok(MarketId::spot(market.market_index));
             }
-            _ => (),
         }
-
         Err(())
     }
     /// Id of a perp market
@@ -186,6 +183,8 @@ pub enum SdkError {
     Rpc(#[from] solana_client::client_error::ClientError),
     #[error("{0}")]
     Ws(#[from] solana_client::nonblocking::pubsub_client::PubsubClientError),
+    #[error("{0}")]
+    Anchor(#[from] anchor_lang::error::Error),
     #[error("error while deserializing")]
     Deserializing,
     #[error("invalid drift account")]
